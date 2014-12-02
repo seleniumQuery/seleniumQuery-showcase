ad6889293e0f80b89fec539e46eafc7c044d116b
acdcjunior <acdcjunior@gmail.com>
2014-12-02 17:16:35 -0200

    Updated demo to match the 0.9.0 released version

--- src/main/java/other/SeleniumQueryExample.java
+++ src/main/java/other/SeleniumQueryExample.java
@@ -4,25 +4,20 @@ import static io.github.seleniumquery.SeleniumQuery.$; // this will allow the sh
 
 public class SeleniumQueryExample {
   public static void main(String[] args) {
-    // sets Firefox as the driver -- this is optional, if omitted, will default
-    // to HtmlUnit or whatever you set at the, also optional, config files
-    //
-    $.driver().useFirefox().withoutJavaScript(); // JS will be disabled!
+    // sets Firefox as the driver (this is optional, if omitted, will default to HtmlUnit)
+    $.driver().useFirefox().withoutJavaScript(); // JavaScript will be disabled!
 
     $.url("http://www.google.com/?hl=en");
 
-    $(":text[name='q']").val("selenium"); // the keys are actually typed
+    $(":text[name='q']").val("selenium"); // the keys are actually typed!
     $(":button:contains('Google Search')").click();
-
-    String resultsText = $("#resultStats").text();
-    System.out.println(resultsText);
+    // Another way: $(":text[name='q']").val("selenium").submit();
 
     // Besides the short syntax and the jQuery behavior you already know,
     // other very useful function in seleniumQuery is .waitUntil(),
-    // handy for dealing with Ajax enabled pages:
-    //
-    $(":input[name='q']").waitUntil().is(":enabled");
-    // The line above waits for no time, as that input is always enabled in google.com
+    // handy for dealing with user-waiting actions (specially in Ajax enabled pages):
+    String resultsText = $("#resultStats").waitUntil().is(":visible").then().text();
+    System.out.println(resultsText);
 
     $.quit(); // quits the currently used driver (firefox)
   }
